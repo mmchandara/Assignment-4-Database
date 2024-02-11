8f78dff9847d19199598132180fb370c66cf0433
mmchandara <mitchel.chandara@gmail.com>
2024-02-11 07:01:55 -0600

    Add project files.

---
+++ Assignment 4/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Assignment_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textID = new System.Windows.Forms.TextBox();
+            this.textFirst = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textLast = new System.Windows.Forms.TextBox();
+            this.textDob = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(452, 395);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(163, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Contacts";
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(594, 68);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(194, 20);
+            this.textID.TabIndex = 2;
+            // 
+            // textFirst
+            // 
+            this.textFirst.Location = new System.Drawing.Point(594, 114);
+            this.textFirst.Name = "textFirst";
+            this.textFirst.Size = new System.Drawing.Size(193, 20);
+            this.textFirst.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(590, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Add Contact Info";
+            // 
+            // textLast
+            // 
+            this.textLast.Location = new System.Drawing.Point(593, 162);
+            this.textLast.Name = "textLast";
+            this.textLast.Size = new System.Drawing.Size(194, 20);
+            this.textLast.TabIndex = 5;
+            // 
+            // textDob
+            // 
+            this.textDob.Location = new System.Drawing.Point(593, 209);
+            this.textDob.Name = "textDob";
+            this.textDob.Size = new System.Drawing.Size(194, 20);
+            this.textDob.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(496, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "First Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(496, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Last Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(496, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Phone";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(528, 319);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(97, 26);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save Data";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(657, 319);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(97, 26);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Update Data";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(528, 368);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(97, 26);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Delete Data";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(657, 368);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(97, 26);
+            this.btnDisplay.TabIndex = 14;
+            this.btnDisplay.Text = "Display Data";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(496, 212);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Date of Birth";
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(593, 258);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.Size = new System.Drawing.Size(194, 20);
+            this.textPhone.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textDob);
+            this.Controls.Add(this.textLast);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textFirst);
+            this.Controls.Add(this.textID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.TextBox textFirst;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textLast;
+        private System.Windows.Forms.TextBox textDob;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textPhone;
+    }
+}
+
---
+++ Assignment 4/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Assignment_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void DisplayContacts()
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;" +
+                "pwd=;database=contactsdb";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                string query = "SELECT * FROM contacts";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                dataGridView1.DataSource = table;
+                
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;" +
+                "pwd=;database=contactsdb";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                string query = "INSERT INTO contacts (firstname, lastname, dob, phone) VALUES (@firstname, @lastname, @dob, @phone)";
+                MySqlCommand save = new MySqlCommand(query, conn);
+                save.Parameters.AddWithValue("@firstname", textFirst.Text);
+                save.Parameters.AddWithValue("@lastname", textLast.Text);
+                save.Parameters.AddWithValue("@dob", DateTime.Parse(textDob.Text));
+                save.Parameters.AddWithValue("@phone", textPhone.Text);
+                save.ExecuteNonQuery();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            DisplayContacts();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;" +
+                "pwd=;database=contactsdb";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                string query = "UPDATE contacts SET firstname=@firstname, lastname=@lastname, dob=@dob, phone=@phone WHERE contactid=@contactid";
+                MySqlCommand update = new MySqlCommand(query, conn);
+                update.Parameters.AddWithValue("@firstname", textFirst.Text);
+                update.Parameters.AddWithValue("@lastname", textLast.Text);
+                update.Parameters.AddWithValue("@dob", DateTime.Parse(textDob.Text));
+                update.Parameters.AddWithValue("@phone", textPhone.Text);
+                update.Parameters.AddWithValue("@contactid", textID.Text);
+                update.ExecuteNonQuery();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            DisplayContacts();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;" +
+                "pwd=;database=contactsdb";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                string query = "DELETE FROM contacts WHERE contactid=@contactid";
+                MySqlCommand delete = new MySqlCommand(query, conn);
+                delete.Parameters.AddWithValue("@contactid", textID.Text);
+                delete.ExecuteNonQuery();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            DisplayContacts();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            DisplayContacts();
+        }
+    }
+}
